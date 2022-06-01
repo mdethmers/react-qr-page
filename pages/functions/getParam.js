64bb52350d7415a3c832008ea18827598fb7589e
Matthijs D <i.m.dethmers@student.utwente.nl>
2022-06-01 13:56:00 +0200

    Window  build error fix

--- pages/functions/getParam.js
+++ pages/functions/getParam.js
@@ -1,8 +1,11 @@
 
 function getParam(param){
-    let params = new URLSearchParams(window.location.search);
-    let foo = params.get(param);
-    return foo;
+    if (typeof window !== 'undefined') {
+        let params = new URLSearchParams(window.location.search);
+        let foo = params.get(param);
+        return foo;
+      }
+
 }
 
 export default getParam;
